83a368be20e128ac0030f612ba2607a92bf87fbc
Krzysztof <krz.glowka@gmail.com>
2019-03-15 16:47:27 +0100

    Add auth communication with the extension

---
+++ src/ExtensionUserManager.js
@@ -0,0 +1,49 @@
+class ExtensionUserManager {
+	port;
+	// TODO use dev for now, in the future set in agreement with the environment
+	devExtensionId = 'lkdlhhnnkbhhnocdodbeikfboeckpaih';
+	prodExtensionId = 'afephghdinbdpfmfdkgbhbolflhnbbdf';
+	portName = 'DASHBOARD';
+	callbacks;
+	
+	constructor() {
+		this.callbacks = [];
+	}
+	
+	init() {
+		this.port = chrome.runtime.connect(this.devExtensionId, {name: this.portName});
+		this.port.onMessage.addListener(this.extensionMessageHandler);
+	}
+	
+	addUserUpdatedListener(callback) {
+		this.callbacks.push(callback);
+	}
+	
+	extensionMessageHandler = (request, sender, sendResponse) => {
+		if (request.action === 'UPDATE_LOGIN_DATA') {
+			const user = request.payload;
+			this.onUserDataChanged(user);
+		}
+	}
+	
+	onUserDataChanged(user) {
+		for (const callback of this.callbacks) {
+			callback(user);
+		}
+	}
+	
+	getPort() {
+		if (!this.port) {
+			throw new Error('Port connection with dashboard not initiated');
+		}
+		return this.port;
+	}
+	
+	pollForUserData() {
+		this.getPort().postMessage({
+			action: 'GET_LOGIN_DATA',
+		});
+	}
+}
+
+export default ExtensionUserManager;
--- src/application/index.jsx
+++ src/application/index.jsx
@@ -1,37 +1,56 @@
-import { hot } from 'react-hot-loader';
+import {hot} from 'react-hot-loader';
 import * as React from 'react';
 
 import 'moment/locale/pl';
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import styles from './index.scss';
 
 import Navigation from '../components/Navigation'
 import PageAnnotationsFeed from '../pages/PageAnnotationsFeed'
 import PageUserAnnotations from '../pages/PageUserAnnotations'
+import ExtensionUserManager from 'ExtensionUserManager';
 
 
-class Dasboard extends React.Component {
-
+class Dashboard extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			user: null,
+		};
+		this.userManager = new ExtensionUserManager();
+	}
+	
+	componentDidMount() {
+		this.userManager.addUserUpdatedListener(this.onLogin);
+		this.userManager.init();
+		this.userManager.pollForUserData();
+	}
+	
+	onLogin = (user) => {
+		this.setState({user: user});
+	}
+	
 	render() {
 		return (
 			<Router>
 				<div className={styles.panel}>
 					<div className={styles.topBar}>
-						<Navigation />
+						<Navigation user={this.state.user}/>
 					</div>
 					<div className={styles.pageContainer}>
-						<Switch >
+						<Switch>
 							<Route
 								exact
 								path="/"
-								render={(props) => <PageAnnotationsFeed {...props} />}
+								render={(props) => <PageAnnotationsFeed {...props} user={this.state.user}/>}
 							/>
 							<Route
 								exact
 								path="/userAnnotations"
-								render={(props) => <PageUserAnnotations {...props} />}
+								render={(props) => <PageUserAnnotations {...props} user={this.state.user}/>}
 							/>
 						</Switch>
 					</div>
@@ -40,4 +59,5 @@ class Dasboard extends React.Component {
 		);
 	}
 }
-export default hot(module)(Dasboard);
+
+export default hot(module)(Dashboard);
